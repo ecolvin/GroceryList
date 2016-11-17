f1a98d9aad63442ba04fb2363bc93d8837f0e12b
ecolvin <ecolvin@mail.csuchico.edu>
2016-11-16 16:50:56 -0800

    Add files via upload

--- groceries.js
+++ groceries.js
@@ -1,12 +1,40 @@
+var myList = [];
+
 function addItem()
 {
   var input = document.getElementById("newItem").value;
-  var list = document.getElementById("listDisplay");
-  var item = document.createElement("li");
-  var itemName = document.createTextNode(input);
-  item.appendChild(itemName);
-  list.appendChild(item);
+  if(myList.indexOf(input) == -1)
+  {
+    myList.push(input);
+    console.log(myList);
+    var list = document.getElementById("listDisplay");
+    var item = document.createElement("li");
+    var btnClose = document.createElement("button");
+    btnClose.classList.add("btn");
+    btnClose.classList.add("btn-danger");
+    btnClose.classList.add("btn-xs");
+    var iconClose = document.createElement("span");
+    iconClose.classList.add("glyphicon");
+    iconClose.classList.add("glyphicon-remove");
+    var itemName = document.createTextNode(input);
+    item.appendChild(itemName);
+    btnClose.appendChild(iconClose);
+    btnClose.addEventListener("click", removeParentListItem);
+    item.appendChild(btnClose);
+    list.appendChild(item);
+  }
   document.getElementById("newItem").value = "";
+}
+
+function removeParentListItem()
+{
+  var mom = this.parentNode;
+  itemRemove = mom.firstChild.textContent;
+  itemIndex = myList.indexOf(itemRemove);
+  myList.splice(itemIndex, 1);
+  console.log(myList);
+  var grandma = mom.parentNode;
+  grandma.removeChild(mom);
 }
 
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
